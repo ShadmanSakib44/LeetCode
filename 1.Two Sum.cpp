8c007650836c53554cd2bf0b004c20533d3c95c3
Shadman Sakib <112627259+ShadmanSakib44@users.noreply.github.com>
2022-10-22 11:15:59 +0600

    Add files via upload

---
+++ 1.Two Sum.cpp
@@ -0,0 +1,21 @@
+//Easy 
+//https://leetcode.com/problems/two-sum/description/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            
+            for (int j = 0; j < n; j++) {
+                if (i != j && nums[i] + nums[j] == target) {
+                    
+                    return {i, j};
+                }
+            }
+        }
+        
+        return {};
+    }
+};
